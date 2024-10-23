f90610e8c2cfb3cd78fa76f79ddb225b3308eb0d
FatemaMarhoon <118673837+FatemaMarhoon@users.noreply.github.com>
2024-10-23 12:11:00 +0300

    user dto

---
+++ src/main/java/com/example/Project4/dto/UserDto.java
@@ -0,0 +1,76 @@
+package com.example.Project4.dto;
+
+import com.example.Project4.model.User;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class UserDto implements Serializable {
+    private Long id;
+    private String email;
+    private String firstName;
+    private String lastName;
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private String password;
+    private Boolean status;
+    @JsonIgnore
+    private String verificationCode;
+    @JsonIgnore
+    private boolean enabled;
+    @JsonIgnore
+    private String resetToken;
+    @JsonIgnore
+    private LocalDateTime tokenExpirationTime;
+    private RoleDto role;
+    private ProfileDto profile;
+
+
+    public UserDto(User user, boolean details) {
+        this.id = user.getId();
+        this.email = user.getEmail();
+        this.password = user.getPassword();
+        this.status = user.getStatus();
+        this.firstName= user.getFirstName();
+        this.lastName= user.getLastName();
+        this.role = user.getRole() != null ? new RoleDto(user.getRole(), false) : null;
+
+        // Only instantiate ProfileDto if details flag is true
+        if (details) {
+            this.profile = user.getProfile() != null ? new ProfileDto(user.getProfile(), false) : null;
+        }
+
+        this.verificationCode = user.getVerificationCode();
+        this.enabled = user.isEnabled();
+        this.resetToken = user.getResetToken();
+        this.tokenExpirationTime = user.getTokenExpirationTime();
+
+
+    }
+
+    @Override
+    public String toString() {
+        return "UserDto{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", role=" + role +
+                ", profile=" + profile +
+                // Add other fields here
+                '}';
+    }
+
+
+}
