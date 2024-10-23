79053d42deb674eb1ee3c6f1d3cc0c54bcd50521
FatemaMarhoon <118673837+FatemaMarhoon@users.noreply.github.com>
2024-10-23 12:12:21 +0300

    Login request and response - all login and registration methods are done

---
+++ src/main/java/com/example/Project4/model/LoginRequest.java
@@ -0,0 +1,15 @@
+package com.example.Project4.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class LoginRequest {
+    private String email;
+    private String password;
+}
---
+++ src/main/java/com/example/Project4/model/LoginResponse.java
@@ -0,0 +1,14 @@
+package com.example.Project4.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class LoginResponse {
+    private String response;
+}
