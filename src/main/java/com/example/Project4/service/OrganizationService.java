d840e5427d29dfa002409cd6c207002f802b5110
FatemaMarhoon <118673837+FatemaMarhoon@users.noreply.github.com>
2024-10-28 09:57:21 +0300

    initialize the creation with a pending status; and added method to approving, rejecting , and getting the approved orgs.

--- src/main/java/com/example/Project4/service/OrganizationService.java
+++ src/main/java/com/example/Project4/service/OrganizationService.java
@@ -4,6 +4,7 @@ import com.example.Project4.model.Organization;
 import com.example.Project4.repository.OrganizationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import com.example.Project4.enums.Status;
 
 import java.util.List;
 import java.util.Optional;
@@ -18,9 +19,31 @@ public class OrganizationService {
         this.organizationRepository = organizationRepository;
     }
 
-    // Create a new organization
+    // Create organization with "Pending" status
     public Organization createOrganization(Organization organization) {
+        organization.setStatus(Status.PENDING);
         return organizationRepository.save(organization);
+    }
+
+    // Approve organization
+    public Organization approveOrganization(Integer organizationId) {
+        Organization organization = organizationRepository.findById(organizationId)
+                .orElseThrow(() -> new RuntimeException("Organization not found"));
+        organization.setStatus(Status.APPROVED);
+        return organizationRepository.save(organization);
+    }
+
+    // Reject organization
+    public Organization rejectOrganization(Integer organizationId) {
+        Organization organization = organizationRepository.findById(organizationId)
+                .orElseThrow(() -> new RuntimeException("Organization not found"));
+        organization.setStatus(Status.REJECTED);
+        return organizationRepository.save(organization);
+    }
+
+    // Only retrieve approved organizations
+    public List<Organization> getAllApprovedOrganizations() {
+        return organizationRepository.findByStatus(Status.APPROVED);
     }
 
     // Get all organizations
