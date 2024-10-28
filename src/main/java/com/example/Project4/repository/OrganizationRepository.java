820e41f6b68913e1eef3ed53040347f0614940d5
FatemaMarhoon <118673837+FatemaMarhoon@users.noreply.github.com>
2024-10-28 09:56:01 +0300

    find by status method in org

--- src/main/java/com/example/Project4/repository/OrganizationRepository.java
+++ src/main/java/com/example/Project4/repository/OrganizationRepository.java
@@ -1,9 +1,14 @@
 package com.example.Project4.repository;
 
+import com.example.Project4.enums.Status;
 import com.example.Project4.model.Organization;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface OrganizationRepository extends JpaRepository<Organization, Integer> {
+    List<Organization> findByStatus(Status status);  // Use Status directly, not Organization.Status
+
 }
