ad0cc2f97da6c04bcfc3d05823007b899204fa3f
FatemaMarhoon <118673837+FatemaMarhoon@users.noreply.github.com>
2024-10-31 15:20:21 +0300

    adding storage config props

--- src/main/java/com/example/Project4/Project4Application.java
+++ src/main/java/com/example/Project4/Project4Application.java
@@ -1,13 +1,28 @@
 package com.example.Project4;
 
+import com.example.Project4.storage.StorageProperties;
+import com.example.Project4.storage.StorageService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
+
 public class Project4Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Project4Application.class, args);
+
+	}
+	@Bean
+	CommandLineRunner init(StorageService storageService) {
+		return (args) -> {
+			storageService.deleteAll();
+			storageService.init();
+		};
 	}
 
 }
