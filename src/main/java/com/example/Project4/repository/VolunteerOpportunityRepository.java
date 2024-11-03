c2bcb20c75776f76835da1ffd1ad4553151ce239
FatemaMarhoon <118673837+FatemaMarhoon@users.noreply.github.com>
2024-11-03 12:27:35 +0300

    added the method in repo

--- src/main/java/com/example/Project4/repository/VolunteerOpportunityRepository.java
+++ src/main/java/com/example/Project4/repository/VolunteerOpportunityRepository.java
@@ -4,9 +4,12 @@ import com.example.Project4.model.VolunteerOpportunity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Repository
 public interface VolunteerOpportunityRepository extends JpaRepository<VolunteerOpportunity, Long> {
     List<VolunteerOpportunity> findByTitleContainingIgnoreCase(String title);
+    List<VolunteerOpportunity> findByEndDateBeforeAndIsArchivedFalse(LocalDate date);
+
 }
